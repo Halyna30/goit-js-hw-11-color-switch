9672134d283d4a19bcec9f16e49f6ebc43850dde
Halyna Pryn <prynhalyna@gmail.com>
2020-12-14 13:04:54 +0100

    Working js-code task 1

--- src/index.js
+++ src/index.js
@@ -1,58 +1,49 @@
 import _ from "lodash";
 import "./style.css";
 
-// const colors = [
-//   "#FFFFFF",
-//   "#2196F3",
-//   "#4CAF50",
-//   "#FF9800",
-//   "#009688",
-//   "#795548",
-// ];
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
 
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-// const refs = {
-//   start: document.querySelector("[data-action='start']"),
-//   stop: document.querySelector("[data-action='stop']"),
-//   body: document.querySelector("body"),
-// };
+const refs = {
+  start: document.querySelector("[data-action='start']"),
+  stop: document.querySelector("[data-action='stop']"),
+  body: document.querySelector("body"),
+};
 
-// refs.start.addEventListener("click", changeColorOn);
+const changeColor = {
+  intervalId: null,
+  changeColorActive: false,
+  on() {
+    if (this.changeColorActive) {
+      return;
+    }
 
-// refs.stop.addEventListener("click", changeColorOn);
+    this.changeColorActive = true;
 
-// function changeColorOn(event) {
-//   console.log(event.target);
-//   console.dir(event);
-//   console.log(event.currentTarget);
-//   // if (event.target === refs.stop) {
-//   //   clearInterval(intervalId);
-//   //   return;
-//   // }
-//   setInterval(() => {
-//     console.log("Change color");
-//   }, 1000);
-// }
-
-// // function changeColorOff(event) {
-// //   clearInterval(intervalId);
-// //   console.log(event.target);
-// // }
+    this.intervalId = setInterval(() => {
+      console.log("change color");
+      refs.body.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length)];
+    }, 1000);
+  },
+  off() {
+    console.log("stop change");
+    this.changeColorActive = false;
+    clearInterval(this.intervalId);
+  },
+};
 
 console.log("Переключатель цветов");
+refs.start.addEventListener("click", changeColor.on.bind(changeColor));
 
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  return element;
-}
-
-document.body.appendChild(component());
-console.log("hello");
+refs.stop.addEventListener("click", changeColor.off.bind(changeColor));
--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,9 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
 };
